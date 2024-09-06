5b91e12693df49ae41442076204ef634d705cb5c
OlimpiaDev16 <olimpia.developer@gmail.com>
2024-09-06 11:24:52 -0400

    Initial Commit

---
+++ Stocks.CashApp/Stocks_CashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stocks_CashAppApp.swift
+//  Stocks.CashApp
+//
+//  Created by Olimpia Compean on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Stocks_CashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Stocks.CashAppUITests/Stocks_CashAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Stocks_CashAppUITestsLaunchTests.swift
+//  Stocks.CashAppUITests
+//
+//  Created by Olimpia Compean on 9/6/24.
+//
+
+import XCTest
+
+final class Stocks_CashAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
